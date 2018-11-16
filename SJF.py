27d79ceaf3333c57de0242ee9f8d3c950cd9677f
fabid009 <44068381+fabid009@users.noreply.github.com>
2018-11-16 23:56:20 +0500

    Update SJF.py

--- SJF.py
+++ SJF.py
@@ -11,20 +11,38 @@ def returnIndexOfLowesetBurst():
     pexecute[min] = 1
     return min
 
-
-print("Enter the number of process:")
-n = int(input())
-processes = []
+# print("Enter the number of process:")
+# n = int(input())
+process = []
 pexecute = []
 burstt = []
 arrivalt = []
 
-for i in range(0,n):
-	processes.insert(i,i+1)
-        pexecute.insert(i,0)
-        burstt.insert(i,int(raw_input("Enter burst time: ")))
-        arrivalt.insert(i,int(raw_input("Enter arrival Time: ")))
-	print("")
+n=0
+a="p0"
+b=1
+c=2
+inp=open("Input.txt","r")
+for line in inp:
+	a,b,c=line.split()
+	process.append(a)
+	pexecute.append(0)
+	burstt.append(int(b))
+	arrivalt.append(int(c))
+	
+n=len(process)
+print("Input read from file: ")
+print("Process: ",process)
+print("BurstT:  ",burstt)
+print("Arrival: ",arrivalt)
+print("")
+
+#for i in range(0,n):
+#	processes.insert(i,i+1)
+#       pexecute.insert(i,0)
+#       burstt.insert(i,int(raw_input("Enter burst time: ")))
+#       arrivalt.insert(i,int(raw_input("Enter arrival Time: ")))
+#	print("")
 
 sum = 0
 waitingt = []
