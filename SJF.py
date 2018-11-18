e93f3b839d2984465f753088ce3f346abf7c590f
fabid009 <44068381+fabid009@users.noreply.github.com>
2018-11-18 19:50:23 +0500

    Update SJF.py

--- SJF.py
+++ SJF.py
@@ -1,18 +1,18 @@
-def returnFirstbursttime():
+def shortest_burst_time():
     for i in range(0,len(burstt)):
         if pexecute[i] != 1:
             return i
 
-def returnIndexOfLowesetBurst():
-    min = returnFirstbursttime()
-    for i in range(returnFirstbursttime() + 1,len(burstt)):
+def Index_of_lowest_bursttime():
+    min = shortest_burst_time()
+    for i in range(shortest_burst_time() + 1,len(burstt)):
         if burstt[i] < burstt[min] and pexecute[i] != 1:
             min = i
     pexecute[min] = 1
     return min
 
-# print("Enter the number of process:")
-# n = int(input())
+#print("Enter the number of process:")
+#n = int(input())
 process = []
 pexecute = []
 burstt = []
@@ -27,18 +27,18 @@ for line in inp:
 	a,b,c=line.split()
 	process.append(a)
 	pexecute.append(0)
-	burstt.append(int(b))
-	arrivalt.append(int(c))
+	arrivalt.append(int(b))
+	burstt.append(int(c))
 	
 n=len(process)
-print("Input read from file: ")
-print("Process: ",process)
-print("BurstT:  ",burstt)
-print("Arrival: ",arrivalt)
+print("Process: " + str(process))
+print("ArrivalT: " + str(arrivalt))
+print("BurstT:  " + str(burstt))
 print("")
 
-#for i in range(0,n):
-#	processes.insert(i,i+1)
+
+#for i in range(0,n):  #for input from user
+#	process.insert(i,i+1)
 #       pexecute.insert(i,0)
 #       burstt.insert(i,int(raw_input("Enter burst time: ")))
 #       arrivalt.insert(i,int(raw_input("Enter arrival Time: ")))
@@ -51,7 +51,7 @@ turnAroundt = []
 waitingt.insert(0,0)
 
 for i in range(0,n):
-	proces = returnIndexOfLowesetBurst()
+	proces = Index_of_loweset_bursttime()
 	runningProcess.insert(i,proces+1)
         sum += burstt[proces]
 	waitingt.insert(i+1,sum)
@@ -63,12 +63,9 @@ sum2 = 0.0
 for i in range(0,len(waitingt) - 1):
 	sum1 += waitingt[i] - arrivalt[i]
 	sum2 += turnAroundt[i]
-	print("Waiting time of P" + str(runningProcess[i]) + " is "+ str(waitingt[i] - arrivalt[i]))
-	print("Arrival time of P" + str(runningProcess[i]) + " = "+ str(turnAroundt[i]))
+	print("Waiting time of P" + str(runningProcess[i]) + " = "+ str(waitingt[i] - arrivalt[i]))
+	print("Turnaround time of P" + str(runningProcess[i]) + " = "+ str(turnAroundt[i]))
 	print("")
 
 print("Avg WaitingTime = " + str(float(float(sum1) / n)))
 print("Avg TurnAroundTime = " + str(float(float(sum2) / n)))
-
-
-
