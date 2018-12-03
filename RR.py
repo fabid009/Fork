27259e90b756c413f0f47405389bfb35991e1869
fabid009 <44068381+fabid009@users.noreply.github.com>
2018-12-03 23:51:31 +0500

    Update RR.py

--- RR.py
+++ RR.py
@@ -1,73 +1,94 @@
+def sort():
+	for i in range(0,n):
+	       	sorted_queue.append([arrival_time[i],burst_time[i],process[i]])
+	sorted_queue.sort()
+	for i in range(0,n):
+		arrival_time[i] = sorted_queue[i][0]
+		burst_time[i] = sorted_queue[i][1]
+		process[i] = sorted_queue[i][2]
+	print(" ")
+
+def not_complete():
+	done = 0
+	for i in range(0,n):
+		if(sorted_queue[i][1]<=0):
+			done = done + 1
+	if(done == n):
+		return 'false'
+	else:
+		return 'true'
 
 process=[]
-readyQ=[]
-bursttime=[]
-rem_btime=[]
-arrivaltime=[]
-completetime=[]
+ready_queue=[]
+sorted_queue=[]
+burst_time=[]
+arrival_time=[]
 turnaroundtime=[]
 
 n = 0
-time_q = 0
 a = "p0"
 b = 1
 c = 2
+
+time_q = 0
 inp = open("Input.txt","r")
 for line in inp:
 	a,b,c=line.split()
 	process.append(a)
-	arrivaltime.append(int(b))
-	bursttime.append(int(c))
+	arrival_time.append(int(b))
+	burst_time.append(int(c))
 	turnaroundtime.append(0)
 
 n = len(process)
 print("Input read from file: ")
 print("Process: "+str(process))
-print("Arrival: "+str(arrivaltime))
-print("BurstT:  "+str(bursttime))
+print("Arrival: "+str(arrival_time))
+print("BurstT:  "+str(burst_time))
 print("")
 
 print ("Enter Time Quantum : ")
 time_q = int(input())
 
 clock = 0
-temparrt=[]
+sort()
 
-execution_complete = 'false'
-while(execution_complete == 'false'):
-	execution_complete = 'true'
+print("After Swapping: ")
+print("Process: " , sorted_queue)
+
+while(not_complete() == 'true'):
 	k=0
-	for j in range( 0,len(process) ):
-		if(arrivaltime[j] != -1):
-			if(arrivaltime[j] <= clock):
-				readyQ.append(bursttime[j])		
-				rem_btime.append(bursttime[j])
-				temparrt.append(arrivaltime[j])
-				completetime.append(0)
-				arrivaltime[j]=-1	
-				if(k == 0):
-					k=j
-	for i in range( k,len(readyQ) ):
-		if(rem_btime[i] > 0):
-			execution_complete = 'false'
-			if(rem_btime[i] > time_q):
-				clock = clock + time_q
-				rem_btime[i] = rem_btime[i]-time_q
-			else:
-				clock = clock + rem_btime[i]
-				completetime[i] = clock
-				rem_btime[i] = 0
-
-
-for i in range(0,len(readyQ)):
-	turnaroundtime[i] = completetime[i] - temparrt[i]
+	y=0
+	count=0
+	for i in range( 0,n ):
+		if(arrival_time[i]>clock):
+			count = count + 1
+	if(count == n):
+		clock = clock + 1
+	else:
+		for j in range( 0,n ):
+			if(sorted_queue[j][0] != -1):
+				if(sorted_queue[j][0] <= clock):
+					ready_queue.append([sorted_queue[j][1],sorted_queue[2]])		
+					sorted_queue[j][0] = -1
+					if(k == 0):
+						k = j
+		for i in range( 0, n ):
+			if(sorted_queue[i][0] == -1):
+				if(sorted_queue[i][1] > 0):
+					if(sorted_queue[i][1] > time_q):
+						clock = clock + time_q
+						sorted_queue[i][1] = sorted_queue[y][1] - time_q
+					else:
+						clock = clock + sorted_queue[i][1]
+						turnaroundtime.insert(i,clock - arrival_time[i])
+						sorted_queue[i][1] = 0
 
 sum_waiting = 0.0
 sum_turnaroundtime = 0.0
 
 for i in range(0,n): 
         sum_turnaroundtime = sum_turnaroundtime + turnaroundtime[i]
-	print("Turnaround time of " + str(process[i]) + " = " + str(turnaroundtime[i]))
+	print("Turnaround time of " + str(sorted_queue[i][2]) + " = " + str(turnaroundtime[i]))
 	print("")
   
 print("Average Turnaround time = " + str(float(float(sum_turnaroundtime) / n)))
