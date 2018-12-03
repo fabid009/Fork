2531d5791981658686e7488bfd5e674c0a74bb36
fabid009 <44068381+fabid009@users.noreply.github.com>
2018-12-03 22:02:07 +0500

    Update PriorityS.py

--- PriorityS.py
+++ PriorityS.py
@@ -1,8 +1,18 @@
+def sort():
+	for i in range(0,n):
+	       	ready_queue.append([priority[i],burst_time[i],process[i]])
+	ready_queue.sort()
+	for i in range(0,n):
+		priority[i] = ready_queue[i][0]
+		burst_time[i] = ready_queue[i][1]
+		process[i] = ready_queue[i][2]
+	print(" ")
 
 process=[]
 priority=[]
-burstt=[]
-pexecute=[]
+burst_time=[]
+ready_queue=[]
+
 n=0
 a="p0"
 b=1
@@ -11,57 +21,45 @@ inp=open("Input.txt","r")
 for line in inp:
 	a,b,c=line.split()
 	process.append(a)
-	pexecute.append(0)
 	priority.append(int(b))
-	burstt.append(int(c))
+	burst_time.append(int(c))
 	
 n=len(process)
 print("Input read from file: ")
 print("Process: " + str(process))
 print("Priority: "+ str(priority))
-print("BurstT:  " + str(burstt))
+print("BurstT:  " + str(burst_time))
 print("")
 
-t_time=[]
-w_time=[]
+sort()
 
-i=0
-j=0
-for i in range(len(priority)-1):
-	for j in range(len(priority)-i-1):
-		if(priority[j]>priority[j+1]):
-			swap=priority[j]
-			priority[j]=priority[j+1]
-			priority[j+1]=swap
+print("After Swapping: ")
+print("Process: " + str(process))
+print("Priority: " + str(priority))
+print("BurstT:  " + str(burst_time))
+print("")
 
-   			swap=burstt[j]
-			burstt[j]=burstt[j+1]
-			burstt[j+1]=swap
+waiting_time=[]
+turn_around_time=[]
 
-			swap=process[j]
-			process[j]=process[j+1]
-			process[j+1]=swap
+waiting_time.insert(0,0)
+turn_around_time.insert(0,burst_time[0])
 
-w_time.insert(0,0)
-t_time.insert(0,burstt[0])
-
-i=1
 for i in range(1,n):
-	w_time.insert(i,w_time[i-1]+burstt[i-1])
-	t_time.insert(i,w_time[i]+burstt[i])
+	waiting_time.insert(i,waiting_time[i-1]+burst_time[i-1])
+	turn_around_time.insert(i,waiting_time[i]+burst_time[i])
 
-sum1=0.0
-sum2=0.0
-i=0
+wsum=0.0
+tsum=0.0
 
-for i in range(0,len(w_time)):
-	print("Waiting time of " + str(process[i]) + " = " + str(w_time[i]))
- 	print("TurnAround time of " + str(process[i]) + " = "+str(t_time[i]))
+for i in range(0,n):
+	print("Waiting time of " + str(process[i]) + " = " + str(waiting_time[i]))
+ 	print("TurnAround time of " + str(process[i]) + " = "+str(turn_around_time[i]))
 	print("")
 
 for i in range(0,n):
-	sum1=sum1+w_time[i]
-	sum2=sum2+t_time[i]
+	wsum=wsum+waiting_time[i]
+	tsum=tsum+turn_around_time[i]
 
-print("Avg WaitingTime = " + str(float(float(sum1) / n)))
-print("Avg TurnAroundTime = " + str(float(float(sum2) / n))) 
+print("Avg WaitingTime = " + str(float(float(wsum) / n)))
+print("Avg TurnAroundTime = " + str(float(float(tsum) / n))) 
